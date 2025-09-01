e07619bdc3d30ea4826266c650ac603fc1c1ce3c
marcos <marquitosgonzalez422@gmail.com>
2025-09-01 16:47:35 -0300

    agregue un programa que pida un numero y este responde si es divisible por 2 o no

---
+++ js/05-divisible2.js
@@ -0,0 +1,7 @@
+const numero = parseInt(prompt("Ingrese un número:"));
+if(numero % 2 === 0){
+    document.writeln("El número " +numero+ " es divisible por 2")
+}else{
+    document.writeln("El número "+numero+" no es divisible por 2")
+}
+    
