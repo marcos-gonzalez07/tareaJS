80d2f5ed778f318d5b6c7aedd44403dcb7b844fa
marcos <marquitosgonzalez422@gmail.com>
2025-09-01 13:05:56 -0300

    práctica de resultado de suma, nro más grande escritos en pantalla

--- js/01-practicaAlert.js
+++ js/01-practicaAlert.js
@@ -4,3 +4,6 @@ alert("un mensaje")
 document.writeln("hello world")
 
 document.writeln(3+5 )
+
+const nombreUsuario = prompt("ingrese su nombre")
+document.writeln("<br> hola "+ nombreUsuario)
---
+++ js/02-practicaSuma.js
@@ -0,0 +1,3 @@
+const nro1 = parseInt(prompt("ingrese nro1"))
+const nro2 = parseInt(prompt("ingrese nro2"))
+document.writeln("<br> el resultado es: "+ (nro1+nro2))
