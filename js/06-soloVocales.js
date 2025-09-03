c0935c142e3475c650035689c1348bb784ab728c
marcos <marquitosgonzalez422@gmail.com>
2025-09-03 17:48:02 -0300

    codigo js para pedir solamente las vocales de una frase

---
+++ js/06-soloVocales.js
@@ -0,0 +1,33 @@
+const frase = prompt("ingrese una frase")
+console.log(frase)
+
+if(frase.charAt(0) === "a" || frase.charAt(0) === "e" || frase.charAt(0) === "i" || frase.charAt(0) === "o" || frase.charAt(0) === "u" || frase.charAt(0) === "A" || frase.charAt(0) === "E" || frase.charAt(0) === "I" || frase.charAt(0) === "O" || frase.charAt(0) === "U"){
+    document.writeln(frase.charAt(0))
+} 
+if(frase.charAt(1) === "a" || frase.charAt(1) === "e" || frase.charAt(1) === "i" || frase.charAt(1) === "o" || frase.charAt(1) === "u" || frase.charAt(1) === "A" || frase.charAt(1) === "E" || frase.charAt(1) === "I" || frase.charAt(1) === "O" || frase.charAt(1) === "U"){
+    document.writeln(frase.charAt(1))
+} 
+if(frase.charAt(2) === "a" || frase.charAt(2) === "e" || frase.charAt(2) === "i" || frase.charAt(2) === "o" || frase.charAt(2) === "u" || frase.charAt(2) === "A" || frase.charAt(2) === "E" || frase.charAt(2) === "I" || frase.charAt(2) === "O" || frase.charAt(2) === "U"){
+    document.writeln(frase.charAt(2))
+} 
+if(frase.charAt(3) === "a" || frase.charAt(3) === "e" || frase.charAt(3) === "i" || frase.charAt(3) === "o" || frase.charAt(3) === "u" || frase.charAt(3) === "A" || frase.charAt(3) === "E" || frase.charAt(3) === "I" || frase.charAt(3) === "O" || frase.charAt(3) === "U"){
+    document.writeln(frase.charAt(3))
+} 
+if(frase.charAt(4) === "a" || frase.charAt(4) === "e" || frase.charAt(4) === "i" || frase.charAt(4) === "o" || frase.charAt(4) === "u" || frase.charAt(4) === "A" || frase.charAt(4) === "E" || frase.charAt(4) === "I" || frase.charAt(4) === "O" || frase.charAt(4) === "U"){
+    document.writeln(frase.charAt(4))
+} 
+if(frase.charAt(5) === "a" || frase.charAt(5) === "e" || frase.charAt(5) === "i" || frase.charAt(5) === "o" || frase.charAt(5) === "u" || frase.charAt(5) === "A" || frase.charAt(5) === "E" || frase.charAt(5) === "I" || frase.charAt(5) === "O" || frase.charAt(5) === "U"){
+    document.writeln(frase.charAt(5))
+} 
+if(frase.charAt(6) === "a" || frase.charAt(6) === "e" || frase.charAt(6) === "i" || frase.charAt(6) === "o" || frase.charAt(6) === "u" || frase.charAt(6) === "A" || frase.charAt(6) === "E" || frase.charAt(6) === "I" || frase.charAt(6) === "O" || frase.charAt(6) === "U"){
+    document.writeln(frase.charAt(6))
+} 
+if(frase.charAt(7) === "a" || frase.charAt(7) === "e" || frase.charAt(7) === "i" || frase.charAt(7) === "o" || frase.charAt(7) === "u" || frase.charAt(7) === "A" || frase.charAt(7) === "E" || frase.charAt(7) === "I" || frase.charAt(7) === "O" || frase.charAt(7) === "U"){
+    document.writeln(frase.charAt(7))
+} 
+if(frase.charAt(8) === "a" || frase.charAt(8) === "e" || frase.charAt(8) === "i" || frase.charAt(8) === "o" || frase.charAt(8) === "u" || frase.charAt(8) === "A" || frase.charAt(8) === "E" || frase.charAt(8) === "I" || frase.charAt(8) === "O" || frase.charAt(8) === "U"){
+    document.writeln(frase.charAt(8))
+} 
+if(frase.charAt(9) === "a" || frase.charAt(9) === "e" || frase.charAt(9) === "i" || frase.charAt(9) === "o" || frase.charAt(9) === "u" || frase.charAt(9) === "A" || frase.charAt(9) === "E" || frase.charAt(9) === "I" || frase.charAt(9) === "O" || frase.charAt(9) === "U"){
+    document.writeln(frase.charAt(9))
+} 
